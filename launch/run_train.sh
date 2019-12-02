5e0cf0efdf4145292171908a8ef671149b97e909
tbfly <tombowfly@gmail.com>
2019-12-02 16:00:23 +0800

    *): Make code run on python3.

--- launch/run_train.sh
+++ launch/run_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 python train.py \
---data_dir path/to/dataset/ \
+--data_dir sample_dataset \
 --name experiment_1 \
 --batch_size 25 \
--- models/models.py
+++ models/models.py
@@ -90,13 +90,13 @@ class BaseModel(object):
             load_path), 'Weights file not found. Have you trained a model!? We are not providing one' % load_path
 
         optimizer.load_state_dict(torch.load(load_path))
-        print 'loaded optimizer: %s' % load_path
+        print('loaded optimizer: %s' % load_path)
 
     def _save_network(self, network, network_label, epoch_label):
         save_filename = 'net_epoch_%s_id_%s.pth' % (epoch_label, network_label)
         save_path = os.path.join(self._save_dir, save_filename)
         torch.save(network.state_dict(), save_path)
-        print 'saved net: %s' % save_path
+        print('saved net: %s' % save_path)
 
     def _load_network(self, network, network_label, epoch_label):
         load_filename = 'net_epoch_%s_id_%s.pth' % (epoch_label, network_label)
@@ -105,7 +105,7 @@ class BaseModel(object):
             load_path), 'Weights file not found. Have you trained a model!? We are not providing one' % load_path
 
         network.load_state_dict(torch.load(load_path))
-        print 'loaded net: %s' % load_path
+        print('loaded net: %s' % load_path)
 
     def update_learning_rate(self):
         pass
